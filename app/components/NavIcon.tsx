844bd9c70a29ee70fe3d4d14a9fc5946f55a60df
Michael Charles Brown <mr.michaelbrown@gmail.com>
2026-01-24 14:34:35 -0800

    Implement mix-blend-mode: difference for nav icon inversion

--- app/components/NavIcon.tsx
+++ app/components/NavIcon.tsx
@@ -10,14 +10,17 @@ export default function NavIcon() {
   const href = pathname === "/" ? "/about" : "/";
 
   return (
-    <nav className="fixed left-0 right-0 z-50 flex justify-center nav-icon-mobile">
+    <nav 
+      className="fixed left-0 right-0 z-50 flex justify-center nav-icon-mobile pointer-events-none"
+      style={{ mixBlendMode: 'difference' }}
+    >
       <Link 
         href={href}
-        className="transition-opacity duration-300 hover:opacity-80"
+        className="transition-opacity duration-300 hover:opacity-80 pointer-events-auto"
         aria-label="Navigation"
       >
         <svg width="42" height="36" viewBox="0 0 42 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M20.7847 0L41.5693 36H5.91278e-05L20.7847 0Z" fill="black"/>
+          <path d="M20.7847 0L41.5693 36H5.91278e-05L20.7847 0Z" fill="white"/>
         </svg>
       </Link>
     </nav>
